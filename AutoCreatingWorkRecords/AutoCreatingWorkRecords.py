37ebf2c6ec4382d1e0d95d06133370c7bfb78f32
morimori-coder <67218438+morimori-coder@users.noreply.github.com>
2021-12-05 19:38:13 +0900

    大まかな処理フローを作成した

--- AutoCreatingWorkRecords/AutoCreatingWorkRecords.py
+++ AutoCreatingWorkRecords/AutoCreatingWorkRecords.py
@@ -1,1 +1,15 @@
 
+# LoginInfo.txtからログイン情報を取得
+# LoginInfo.txtはクローズ
+# chrome driverでGoogle Chromeを開く
+# ログインしたいページのURLを入力する
+# ID,パスワードを入力する
+# seleniumで対象のページへログインする
+# 勤務表タブをクリック
+# 前月リンクをクリック
+# 月次サマリーをクリック
+# Ctrl + Sを実行
+# 勤務表.html としてローカルに保存
+# 勤務表.htmlをbeautifulsoupで開く
+# 勤務表の要素を取得する
+# 取得した要素の値を利用し、Excelを作成する（この辺りは今後やり方を調べる）
