ba13ea6331378fac2880ee3f3030545d9606da6b
morimori-coder <67218438+morimori-coder@users.noreply.github.com>
2021-12-10 09:20:04 +0900

    月次サマリーの画面へ遷移するようになった

--- AutoCreatingWorkRecords/AutoCreatingWorkRecords.py
+++ AutoCreatingWorkRecords/AutoCreatingWorkRecords.py
@@ -3,8 +3,10 @@ from selenium.webdriver.common.keys import Keys
 import json
 import time
 
+
+
 # LoginInfo.txtからログイン情報を取得
-jsonData = open('LoginInfo.json', 'r', encoding='utf=8')
+jsonData = open('Parameters/LoginInfo.json', 'r', encoding='utf=8')
 loginInfo = json.load(jsonData)
 
 # LoginInfo.txtはクローズ
@@ -27,10 +29,32 @@ loginPass.send_keys(loginInfo["password"])
 loginButton = driver.find_element_by_id('Login')
 loginButton.click()
 
-time.sleep(4)
+#time.sleep(4)
 # 勤務表タブをクリック
+workRecordTab = driver.find_element_by_id("01r6F000001hAO5_Tab")
+workRecordTab.click()
+time.sleep(2)
+
+# LoginInfo.txtからログイン情報を取得
+jsonData = open('Parameters/ElementsName.json', 'r', encoding='utf=8')
+elementsNames = json.load(jsonData)
+
+# LoginInfo.txtはクローズ
+jsonData.close()
+
+workRecordTab = driver.find_element_by_id(elementsNames["workedRecord"])
+workRecordTab.click()
+time.sleep(2)
+
+monthSummery = driver.find_element_by_class_name(elementsNames['monthlySum'])
+monthSummery.click()
+time.sleep(2)
+
+driver.quit()
+
 # 前月リンクをクリック
 # 月次サマリーをクリック
+
 # Ctrl + Sを実行
 # 勤務表.html としてローカルに保存
 # 勤務表.htmlをbeautifulsoupで開く
